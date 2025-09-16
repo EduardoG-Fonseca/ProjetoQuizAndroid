f9210930d4ac6f6b02a9e41e559c7e063fe8a47d
EduardoFonseca <eduardo.fonck1308@gmail.com>
2025-09-16 11:11:50 -0300

    com IDs mencionados

--- app/src/main/java/com/example/projetoquiz/MainActivity.java
+++ app/src/main/java/com/example/projetoquiz/MainActivity.java
@@ -42,10 +42,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        txvPergunta = findViewById(R.id.txv_perguntas);
+        rdgPrincipal = findViewById(R.id.rdg_principal);
+        btnResponder = findViewById(R.id.btn_responder);
+
+        // Nosso método!
+        carregarPerguntas();
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+
+    private void carregarPerguntas() {
     }
 }
